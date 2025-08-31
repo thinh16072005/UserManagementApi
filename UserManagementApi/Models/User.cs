a09b6cd87ccdf3c15386607d0ec3c5bc7abcbbc2
hungt <hungthinh16072005@gmail.com>
2025-08-31 16:43:50 +0700

    <add:user-model> Add user model

---
+++ UserManagementApi/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementApi.Models;
+
+public class User
+{
+    [Required]
+    private int Id { get; set; }
+    
+    [Required]
+    private string Name { get; set; }
+    
+    [Required]
+    [StringLength(10, ErrorMessage = "Phone number cannot exceed 10 digits")]
+    private string Phone { get; set; }
+    
+    [Required]
+    private DateOnly Birthdate { get; set; }
+    
+    [Required]
+    private string Address { get; set; }
+}
