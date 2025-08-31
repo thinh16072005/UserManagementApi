72ad58a2735062012a3aa7f229a25c30e08c6212
hungt <hungthinh16072005@gmail.com>
2025-08-31 22:02:47 +0700

    <add>: Add main app file

---
+++ UserManagementApi/Program.cs
@@ -0,0 +1,22 @@
+using UserManagementApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddControllers();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapControllers();
+app.Run();
